e11e1e1659cced1539a87ed9aa4513f2011da213
Anton Georgesco <antongeorgescu@gmail.com>
2020-08-17 18:14:43 -0400

    fixed a bunch of bugs

--- Alvianda.AI.Service.Python/runserver.py
+++ Alvianda.AI.Service.Python/runserver.py
@@ -7,9 +7,10 @@ from WineQuality_RestAPI import app
 
 if __name__ == '__main__':
     try:
+        # GEVENT_SUPPORT = environ.get('GEVENT_SUPPORT',True)
         HOST = environ.get('SERVER_HOST', 'localhost')
         PORT = int(environ.get('SERVER_PORT', '53535'))
-        app.run(HOST, PORT)
+        app.run(HOST, PORT, debug=True)
     except ValueError:
         print("Error getting URL from environment variables.")
     
